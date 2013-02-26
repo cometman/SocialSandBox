bf7cee9c58ae8dd475ef23375da624f551b55b2e
Clay S <csbuiss@gmail.com>
2013-02-25 23:34:02 -0600

    Adding everything in sand box

--- SandBox/ViewController.m
+++ SandBox/ViewController.m
@@ -41,22 +41,18 @@
         if (friends != nil)
             NSLog(@"Friends: %@", [[friends objectAtIndex:0] displayName]);
     }
-    else {
-        [SocialAPI authenticateFB];
-    }
 }
 
 - (IBAction)getProfile:(id)sender {
     if ([SocialAPI isTokenValid] == YES)
     {
         Profile *userProfile = [SocialAPI getProfile];
+        self.resultView.text = userProfile.firstName;
+        
         NSLog(@"Profile: %@", [userProfile description]);
     }
-    else {
-        [SocialAPI authenticateFB];
-    }
 }
-
+//
 - (IBAction)GetLikes:(id)sender {
     if ([SocialAPI isTokenValid] == YES)
     {
@@ -71,70 +67,67 @@
         self.resultView.text = resultString;
 
     }
-    else {
-        [SocialAPI authenticateFB];
-    }
 }
-
-
-
-- (IBAction)getMovies:(id)sender {
-    if ([SocialAPI isTokenValid] == YES)
-    {
-        NSMutableString* resultString = [[NSMutableString alloc] init];
-        NSArray *userMovies = [SocialAPI getMovies];
-        
-        for (int i = 0; i<[userMovies count]; i++)
-        {
-            [resultString appendString:[[userMovies objectAtIndex:i] name]];
-        }
-        
-        self.resultView.text = resultString;
-        
-    }
-    else {
-        [SocialAPI authenticateFB];
-    }
-}
-
-- (IBAction)getMusic:(id)sender {
-    if ([SocialAPI isTokenValid] == YES)
-    {
-        NSMutableString* resultString = [[NSMutableString alloc] init];
-        NSArray *objects = [SocialAPI getMusic];
-        
-        for (int i = 0; i<[objects count]; i++)
-        {
-            [resultString appendString:[[objects objectAtIndex:i] name]];
-        }
-        
-        self.resultView.text = resultString;
-        
-    }
-    else {
-        [SocialAPI authenticateFB];
-    }
-}
-
-- (IBAction)getNotes:(id)sender {
-    if ([SocialAPI isTokenValid] == YES)
-    {
-        [self displayStringForObjects:[SocialAPI getPermissions]];
-    }
-    else {
-        [SocialAPI authenticateFB];
-    }
-}
-
-- (IBAction)getAlbum:(id)sender {
-    if ([SocialAPI isTokenValid] == YES)
-    {
-        [self displayStringForObjects:[SocialAPI getAlbums]];
-    }
-    else {
-        [SocialAPI authenticateFB];
-    }
-}
+//
+//
+//
+//- (IBAction)getMovies:(id)sender {
+//    if ([SocialAPI isTokenValid] == YES)
+//    {
+//        NSMutableString* resultString = [[NSMutableString alloc] init];
+//        NSArray *userMovies = [SocialAPI getMovies];
+//        
+//        for (int i = 0; i<[userMovies count]; i++)
+//        {
+//            [resultString appendString:[[userMovies objectAtIndex:i] name]];
+//        }
+//        
+//        self.resultView.text = resultString;
+//        
+//    }
+//    else {
+//        [SocialAPI authenticateFB];
+//    }
+//}
+//
+//- (IBAction)getMusic:(id)sender {
+//    if ([SocialAPI isTokenValid] == YES)
+//    {
+//        NSMutableString* resultString = [[NSMutableString alloc] init];
+//        NSArray *objects = [SocialAPI getMusic];
+//        
+//        for (int i = 0; i<[objects count]; i++)
+//        {
+//            [resultString appendString:[[objects objectAtIndex:i] name]];
+//        }
+//        
+//        self.resultView.text = resultString;
+//        
+//    }
+//    else {
+//        [SocialAPI authenticateFB];
+//    }
+//}
+//
+//- (IBAction)getNotes:(id)sender {
+//    if ([SocialAPI isTokenValid] == YES)
+//    {
+//        [self displayStringForObjects:[SocialAPI getPermissions]];
+//    }
+//    else {
+//        [SocialAPI authenticateFB];
+//    }
+//}
+//
+//- (IBAction)getAlbum:(id)sender {
+//    if ([SocialAPI isTokenValid] == YES)
+//    {
+//        [self displayStringForObjects:[SocialAPI getAlbums]];
+//    }
+//    else {
+//        [SocialAPI authenticateFB];
+//    }
+//}
 
 -(void) displayStringForObjects:(NSArray* )objects
 {
